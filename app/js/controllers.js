9677f1d770e48909b292472d697bd87991765ada
Fumistar <francis.duffeal@gmail.com>
2014-06-14 09:40:16 +0200

    battle net appel

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+    .controller('MyCtrl1', ['$scope','d3Api', function ($scope,d3Api) {
+        d3Api.get('Fumétoile',2442,10734237);
+    }])
+    .controller('MyCtrl2', ['$scope', function ($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+    }]);
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,21 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+    value('version', '0.1');
+
+var app = angular.module('myApp.services');
+
+app.service('d3Api',['$http',
+    function($http){
+        this.get = function(battletagName,battletagCode,heroId){
+
+            var host = 'eu.battle.net';
+            var url = 'http://'+ host + "/api/d3/profile/" + battletagName + "-" + battletagCode +"/hero/" + heroId;
+
+            return $http({
+                method: 'POST',
+                url: url
+            });
+        };
+    }
+]);

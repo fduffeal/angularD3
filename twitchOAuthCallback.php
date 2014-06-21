c0b2a03b6d973f2ed3de931388dd798dba0baae2
Fumistar <francis.duffeal@gmail.com>
2014-06-21 20:32:19 +0200

    twitch step 2 OAuth

--- twitchOAuthCallback.php
+++ twitchOAuthCallback.php
@@ -27,3 +27,43 @@ We'll respond with a JSON-encoded access token:
   "access_token": "[user access token]",
   "scope":[array of requested scopes]
 }
+*/
+if($_REQUEST['code']){
+    $clientId = "fdb8sc454mtrk3b1dcmavxt5h1468e7";
+    $redirectUrl = "http://".$_SERVER["SERVER_NAME"].'/angularD3/twitchOAuthCallback.php';
+    $clientSecret = "amz8o15rt0zbgsvmqvocqzz5pg3kez7";
+
+    $postdata = http_build_query(
+        array(
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => $redirectUrl,
+            'code' => $_REQUEST['code'],
+        )
+    );
+
+    $opts = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata
+        )
+    );
+
+    $context = stream_context_create($opts);
+
+    $result = file_get_contents('https://api.twitch.tv/kraken/oauth2/token', false, $context);
+
+    var_dump($result);
+
+    /*
+     * {"access_token":"9076vv8rdodrlsxqzad9w20ltbgx1t","refresh_token":"eyJfaWQiOiI0NzEwNzM2NyJ9%W3O19xtsM/ZkkJI+B2BLWZu1u3CLbjDe2SdAq9J4HrM=","scope":["user_read","channel_read"]}"
+     */
+
+
+}
+
+
+
+?>
